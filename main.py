fd65a3d50cb95f800d3b4d4eb56b1987b8835ba2
BrunoBenunes <brunobenuness@hotmail.com>
2023-11-07 18:03:00 -0300

    feat(def(extract_system) Definindo a função de extrato)

--- main.py
+++ main.py
@@ -17,6 +17,13 @@ MAX_WITHDRAW = 500
 balance = 0
 extract = []
 
+def extract_system(list_extract):
+    print('Extrato'.center(10, '#'))
+    for operation in list_extract:
+        print(operation)
+    print(f'##### Saldo Atual: R$ {balance:.2f}')
+
+
 while True:
     command = input(menu)
 
