3aef5efc8f0aab777a13f558af2ad0bc593f7bdc
BrunoBenunes <brunobenuness@hotmail.com>
2023-11-07 18:05:21 -0300

    feat(if conditional command "s", Desenvolvendo o if do comando [s] para Sacar

--- main.py
+++ main.py
@@ -28,7 +28,33 @@ while True:
     command = input(menu)
 
     if (command == 's'):
-        ...
+        if (count_withdraws == DAILY_WITHDRAW):
+            print('Você já atingiu o seu limite máximo de saque DIÁRIO')
+            continue
+
+        amount_withdraw = float(input('Quanto deseja sacar?: '))
+
+        if (amount_withdraw <= balance):
+            print('Sacando....')
+            if (amount_withdraw < 0):
+                print('Falha no Saque, insira um valor POSITIVO')
+                continue
+            elif (amount_withdraw > MAX_WITHDRAW):
+                print(f'Falha no Saque. O valor máximo para SAQUE é de R${MAX_WITHDRAW:.2f}')
+                continue
+
+            balance -= amount_withdraw
+            print()
+            print(f'Saque realizado com SUCESSO, saldo atual: {balance:.2f}')
+            extract.append(f'SAQUE: R$ -{amount_withdraw}')
+            count_withdraws += 1
+            
+        
+        else:
+            print('Sacando....')
+            print()
+            print(f'NÂO possível realizar o SAQUE por falta de saldo. Seu saldo Atual : {balance:.2f}')
+            continue
     
     elif(command == 'd'):
         ...
