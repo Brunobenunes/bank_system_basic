28f51f98a39a048903e89d46bd24fcb780b499a2
BrunoBenunes <brunobenuness@hotmail.com>
2023-11-08 23:36:34 -0300

    Desafio refeito utlizando POO

--- main.py
+++ main.py
@@ -1,256 +1,291 @@
-from copy import deepcopy
+from abc import ABC, abstractclassmethod, abstractproperty
 
-menu_login = '''
-########## Bank DIO ##########
-     #### Bem Vindo(a) ####
-     
-     [e] Entrar
-     [c] Cadastrar Usuário
-     [q] Sair
+CLIENTS = [
 
-'''
+]
+ACCOUNTS = [
 
-options_menu_display = '''
-    ## Bank DIO System ##
+]
+class Transaction(ABC):
 
-    [c] Criar Conta-Corrente
-    [a] Acessar Contas
-    [q] Sair
+    @property
+    @abstractproperty
+    def value(self):
+        pass
+
+    @abstractclassmethod
+    def register(self, account):
+        pass
+
+
+class Client:
+    def __init__(self, address) -> None:
+        self._address = address
+        self._accounts = []
+
+    def run_transaction(self, account, transaction: Transaction):
+        transaction.register(account)
+    
+    def add_account(self, account):
+        self._accounts.append(account)
+
+class Person(Client):
+    def __init__(self, address, cpf, name, birth, password= '') -> None:
+        super().__init__(address)
+        self._cpf = cpf
+        self._name = name
+        self._birth = birth
+        self._password = password
+
+
+    @property
+    def name(self):
+        return self._name
+    
+    @property
+    def accounts(self):
+        return self._accounts
+    
+    def login_check(self, login, password):
+        print(login, password, '///// SELF: ', self._cpf, self._password)
+        return (login == self._cpf and password == self._password), self
+
 
     
-'''
+    @classmethod
+    def create(cls):
+        name = input('Digite seu Nome e Sobrenome: ')
+        cpf = input('Digite seu CPF: ')
+        birth = input('Digite sua Data de Nascimento: dd/mm/aaaa ')
+        street = input('Digite Seu endereço: * Rua/Av, numero* ')
+        district = input('Digite Seu Bairro: ')
+        city = input('Digite sua Cidade: ')
+        state = input('Digite o UF: *MG* ')
+        password = input('Digite sua Senha: ')
+        address = f'{street} - {district}, {city}/{state}'
+        return CLIENTS.append(cls(name=name, cpf=cpf, birth=birth, address=address, password=password))
 
-menu = '''
+class AccountClient:
+    def __init__(self, number, client) -> None:
+        self._balance = 0
+        self._number = number
+        self._branch = '0001'
+        self._client = client
+        self._history = History()
 
-    ## Bank DIO System ##
+    @property
+    def number(self):
+        return self._number
+    
+    @property
+    def checking_account(self):
+        return self._checking_account
+    
+    @property
+    def history(self):
+        return self._history
+    
+    @property
+    def client(self):
+        return self._client
 
-    [s] Sacar
-    [d] Depositar
-    [e] Extrato
-    [q] Sair
+    @property
+    def balance(self):
+        return self._balance
 
-    #####################
-
-'''
-
-DAILY_WITHDRAW = 3
-count_withdraws = 0
-MAX_WITHDRAW = 500
-balance = 0
-extract = []
-
-users = [
-
-]
-
-accounts = [
-]
-
-BASE_ACCOUNT = {
-    'branch': 0o1,
-    'balance': 0,
-    'count_withdraws': 0,
-    'MAX_WITHDRAW': 500,
-    'extract': [],
-    'DAILY_WITHDRAW': 3,
-}
-
-def check_user(cpf):
-    for user in users:
-        if (cpf in user):
+    def withdraw(self, value):
+        if (value < 0):
+            print('@@@@ Falha no Saque. Insira um valor Positivo! @@@')
+            return False
+        if (value > self.balance):
+            print(self.balance)
+            print('@@@@ Falha no Saque. Saldo Inválido! @@@')
+            return False
+        if (value <= self.balance):
+            print('&&&& Saque Realizado com SUCESSO! &&&')
+            self._balance -= value
             return True
-    return False
-
-
-def create_user():
-    cpf = input('Digite seu CPF: ')
-    if (check_user(cpf)):
-        print('\n#'.center(10,'#'))
-        return print('Esse Usuario já existe!')
-    
-    name = input('Digite seu NOME: *Nome e Sobrenome*: ')
-    birth_year = int(input('Digite seu ano de Nascimento: '))
-    street = input('Digite Seu endereço: *RUA e NUMERO e COMPLEMENTO*: ')
-    district = input('Digite Seu Bairro: ')
-    state = input('Digite Seu Estado: ')
-    password = input('Digite sua SENHA: ')
-    cpf_format = ''.join(cpf.split('.'))
-    cpf = ''.join(cpf_format.split('-'))
-
-    new_user = {
-        cpf: {
-            'name': name,
-            'birth_year': birth_year,
-            'adress': f'{street} - {district}/{state}',
-            'password': password,
-            'accounts': []
-        }
-    }
-    return users.append(new_user)
-
-def extract_system(balance, / , *, list_extract):
-    print('Extrato'.center(10, '#'))
-    for operation in list_extract:
-        print(operation)
-    print(f'##### Saldo Atual: R$ {balance:.2f}')
-
-def deposit_system(balance, extract: list):
-    amount_deposit = float(input('Qual o valor você deseja DEPOISTAR?: '))
-    new_balance = 0
-    if (amount_deposit > 0):
-            print('Depositando...')
-            new_balance = balance + amount_deposit
-            print('DEPOSITO efetuado com SUCESSO!')
-            extract.append(f'DEPOSITO: R$ {amount_deposit:.2f}')
-            return new_balance
-    else:
-        print('Falha no Deposito. Insira valores Positivos')
-        return new_balance
-    
-def withdraw_system(**kwargs):
-    extract : list = kwargs['extract']
-    balance = kwargs['balance']
-    amount_withdraw = kwargs['amount_withdraw']
-    new_balance = balance
-
-    if (amount_withdraw <= balance):
-        print('Sacando....')
-            
-        new_balance -= amount_withdraw
-        print(f'\nSaque realizado com SUCESSO, saldo atual: {new_balance:.2f}')
-        extract.append(f'SAQUE: R$ -{amount_withdraw}')
-        return new_balance, extract
         
-    else:
-        print('Sacando....')
-        print(f'\nNÂO possível realizar o SAQUE por falta de saldo. Seu saldo Atual : {balance:.2f}')
-        return new_balance, extract
+    def deposit(self, value):
+        if (value < 0):
+            print('@@@@ Falha no Depósito. Insira Valores Positivos! @@@')
+            return False
+        self._balance += value
+        return True
+    
+    @classmethod
+    def create_account(cls, client, number):
+        ACCOUNTS.append(cls(number=number, client=client))
+        return client.accounts.append(number)
+    
+class ChekingAccount(AccountClient):
+    def __init__(self, number, client) -> None:
+        super().__init__(number, client)
+        self._max_withdraw = 500
+        self._daily_withdraw = 3
+        self._count_withdraw = 0
+    
+    def withdraw(self, value):
+        if (self._count_withdraw == self._daily_withdraw):
+            print('@@@@ Falha no Saque. Você já atingiu o limite máximo de saques diários! @@@@')
+            return False
+        if (value > self._max_withdraw):
+            print(f'@@@@ Falha no Saque. Limite máximo para saque: R$ {self._max_withdraw} @@@@')
+            return False
+        return super().withdraw(value)
 
-def transfer_operations(account_number):
-        account = find_account_with_number(account_number)
-        DAILY_WITHDRAW = account['DAILY_WITHDRAW']
-        count_withdraws = account['count_withdraws']
-        MAX_WITHDRAW = account['MAX_WITHDRAW']
-        balance = account['balance']
-        extract = account['extract']
+
+class Deposit(Transaction):
+    def __init__(self, value) -> None:
+        self._value = value
+
+    @property
+    def value(self):
+        return self._value
+    @value.setter
+    def value(self, value):
+        self._value = value
+
+    def register(self, account: ChekingAccount):
+        if (account.deposit(self.value)):
+            new_Transaction = f'{self.__class__.__name__}: R$ {self._value}'
+            account.history.add_transaction(new_Transaction)
         
-        while True:
-            command = input(menu)
+class Withdraw(Transaction):
+    def __init__(self, value) -> None:
+        self._value = value
 
-            if (command == 's'):
-                if (count_withdraws == DAILY_WITHDRAW):
-                    print('Você já atingiu o seu limite máximo de saque DIÁRIO')
-                    continue
+    @property
+    def value(self):
+        return self._value
+    @value.setter
+    def value(self, value):
+        self._value = value
+    
+    def register(self, account: ChekingAccount):
+        if (account.withdraw(self.value)):
+            new_Transaction = f'{self.__class__.__name__}: R$ -{self._value}'
+            account.history.add_transaction(new_Transaction)
 
-                amount_withdraw = float(input('Quanto deseja sacar?: '))
-                
-                if (amount_withdraw < 0):
-                    print('Falha no Saque, insira um valor POSITIVO')
-                    continue
-                
-                elif (amount_withdraw > MAX_WITHDRAW):
-                    print(f'Falha no Saque. O valor máximo para SAQUE é de R${MAX_WITHDRAW:.2f}')
-                    continue
+class History:
+    def __init__(self) -> None:
+        self._history = []
+    
+    def add_transaction(self, transaction):
+        self._history.append(transaction)
 
-                elif (amount_withdraw <= balance):
-                    balance, extract = withdraw_system(balance=balance, extract=extract, amount_withdraw=amount_withdraw)
-                    count_withdraws += 1
-                    print(balance)
-                    continue
+    def list_transactions(self, account: ChekingAccount):
+        print(f'''
+=======================================================
+            Extrato da conta: {account.number}
+''')
+        for transaction in self._history:
+            print(transaction)
+        print(f'''
+========================== Saldo Atual: {account.balance}
+=======================================================
+''')
 
-            elif(command == 'd'):
-                balance = deposit_system(balance, extract)
-                continue
 
-            if (command == 'e'):
-                extract_system(balance, list_extract=extract)
-                continue
-
-            if (command == 'q'):
-                print('Saindo...')
-                print('\nTenha um ótimo Dia')
-                break
-
-            else:
-                print('Insira um comando válido')
-                continue
-
-def find_account_with_number(account_number):
-    for acc in accounts :
-        if (list(acc.keys())[0] == account_number):
-            return acc[account_number]
-
-def check_user_login(user, password):
-    for user_ in users:
-        if (list(user_.keys())[0] == user):
-            if (user_[user]['password'] == password):
-                return True, user_[user]
-            else:
-                return False
-
-def input_account_choice(user):
-    account_options = []
-    for index, acc in enumerate(user['accounts']):
-        account_options.append(f'[{index + 1}]: {acc}')
-    print('## Bank DIO System ##'.center(10, ' '))
-    print(*account_options, sep='\n')
-    print()
-    return 'Sua Opção: '
-
-def account_choice(user, account):
-    return user['accounts'][int(account) - 1]
-
-def options_menu(user):
+def menu_login():
     while True:
-        command = input(options_menu_display)
-        if (command == 'a'):
-            if (user['accounts']):
-                account = input(input_account_choice(user))
-                if (int(account) > len(user['accounts'])):
-                    print('Conta não encontrada')
-                    continue
-                transfer_operations(account_choice(user, account))
+        command = input('''
+    =======================================================
+                    ## Bank System DiO ##
+                        
+        [e] Entrar
+        [c] Criar Usuario
+        [q] Sair
+                        
+    =======================================================
+    ''')
+        if (command == 'e'):
+            login = input('Digite seu CPF: ')
+            client = ''
+            status = ''
+            password = input('Digite sua SENHA: ')
+            for user in CLIENTS:
+                status, client_login = user.login_check(login, password)
+                if (status):
+                    client = client_login
+            if (status):
+                print(f'Bem vindo {client.name}')
+                client_menu(client)
             else:
-                print('Você ainda não possuiu nenhuma conta!')
-                continue
+                print('@@@@   Usuario ou Senha Inválida!   @@@@')
         elif (command == 'c'):
-            accounts.append(create_accounts(user, BASE_ACCOUNT))
+            Person.create()
+            continue
         elif (command == 'q'):
+            print(' Tenha um Otimo Dia!')
             break
+        else :
+            print('@@@@ Comando Inválido. Tente Novamente! @@@@')
+            continue
 
-def create_accounts(user, BASE_ACCOUNT: dict):
-    new_account = deepcopy(BASE_ACCOUNT)
-    user_id = ''
-    for user_ in users:
-        if (user_[list(user_.keys())[0]]['name'] == user['name']):
-            user_id = list(user_.keys())[0]
-    
-    new_account['user'] = user_id
-    last_account_id = 0
-    if (len(accounts)):
-        last_account_id = len(accounts)
-    account_id = last_account_id
-    user['accounts'].append(account_id)
-    return {
-        account_id : new_account
-    }
+def client_menu(client):
+    while True:
+        command = input(f'''
+    =======================================================
+                    ## Bank System DiO ##
+                        Bem Vindo {client.name}
+                        
+        [a] Acessar Contas
+        [c] Criar Conta
+        [q] Sair
+                        
+    =======================================================
+    ''')
+        
+        if (command == 'a'):
+            print(f'''
+    =======================================================
+                    ## Bank System DiO ##
+                        Bem Vindo {client.name}
+                        
 
-
-
-while True:
-    command = input(menu_login)
-
-    if (command == 'c'):
-        create_user()
-        continue
-    elif (command == 'e'):
-        user = input('Digite seu usuario: *CPF*: ')
-        password = input('Digite sua senha: ')
-        if (check_user_login(user, password)):
-            _, user_loged = check_user_login(user, password)
-            print('Bem vindo!')
-            options_menu(user_loged)
+''')
+            for index, account in enumerate(client.accounts):
+                print(f'[{index}]: Acc: {account}')
+            account_choice = int(input('\n'))
+            account = [acc for acc in ACCOUNTS if acc.number == client.accounts[account_choice]][0]
+            account_menu(client, account)
+        if (command == 'c'):
+            ChekingAccount.create_account(client, len(ACCOUNTS))
+        if (command == 'q'):
+            break
         else:
-            print('SENHA ou USUARIO INCORRETO')
-    elif (command == 'q'):
-        break
+            print('@@@@ Comando Inválido. Tente Novamente! @@@@')
+            continue
+
+def account_menu(client : Client, account : AccountClient):
+    while True:
+        command = input(f'''
+        =======================================================
+                        ## Bank System DiO ##
+                        Bem vindo {client.name}
+                        Acc: {account.number}
+                            
+            [s] Sacar
+            [d] Depositar
+            [e] Extrato
+            [q] Sair
+                            
+        =======================================================
+        ''')
+        if (command == 's'):
+            operation = Withdraw(float(input('Quanto deseja Sacar?:')))
+            client.run_transaction(account, operation)
+
+        if (command == 'd'):
+            operation = Deposit(float(input('Quanto deseja Depositar?: ')))
+            client.run_transaction(account, operation)
+        
+        if (command == 'e'):
+            account.history.list_transactions(account)
+
+        if (command == 'q'):
+            break
+        
+            
+menu_login()
